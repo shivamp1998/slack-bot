2498498adeff60d532c8832787aa17540bad7886
shivamp1998 <shivampandey98@gmail.com>
2024-11-22 22:19:57 +0530

    jira issue creation

--- app.js
+++ app.js
@@ -1,52 +1,95 @@
-const dotenv = require('dotenv').config()
-const express = require('express');
+const dotenv = require("dotenv").config();
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
+const axios = require("axios");
+const { ChatOpenAI } = require("@langchain/openai");
 
-const PORT = process.env.PORT || 2000
+const PORT = process.env.PORT || 2000;
 const token = process.env.BOT_TOKEN;
 
-//slack imports 
-const eventsApi = require('@slack/events-api');
+//slack imports
+const eventsApi = require("@slack/events-api");
 const slackEvents = eventsApi.createEventAdapter(process.env.APP_SIGNINSECRET);
 const { WebClient, LogLevel } = require("@slack/web-api");
-const { ChatOpenAI } = require('@langchain/openai');
 
 const client = new WebClient(token, {
-    logLevel: LogLevel.DEBUG
-})
+  logLevel: LogLevel.DEBUG,
+});
 
 const chatModel = new ChatOpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-})
-//middle wares
-app.use('/', slackEvents.expressMiddleware());
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
+//middlewares
+app.use("/", slackEvents.expressMiddleware());
+app.use((err, req, res, next) => {
+  console.error("Global error handler:", err);
+  res.status(500).send("Something broke!");
+});
 
 const handleCallOpenAi = async (message) => {
-    const response = await chatModel.call([`Reply to this message: ${message}`])
-    console.log(response?.content);
-    return response?.content;
-
-}   
+  const response = await chatModel.call([`Reply to this message: ${message}`]);
+  console.log(response?.content);
+  return response?.content;
+};
 
 slackEvents.on("message", async (event) => {
-    console.log(event?.subtype, event.bot_id)
-    console.log(event);
-    if(!event?.subtype && !event.bot_id) {
-        client.chat.postMessage({
-            token,
-            channel: event.channel,
-            text: await handleCallOpenAi(event.text) || 'hello'
-        })
+  console.log(event?.subtype, event.bot_id);
+  if (!event?.subtype && !event.bot_id) {
+    client.chat.postMessage({
+      token,
+      channel: event.channel,
+      text: handleCallOpenAi(event.text),
+    });
+
+    const data = {
+      fields: {
+        project: {
+          key: "TPOP",
+        },
+        summary: event.text,
+        description: {
+          content: [
+            {
+              content: [
+                {
+                  text: event.text,
+                  type: "text",
+                },
+              ],
+              type: "paragraph",
+            },
+          ],
+          type: "doc",
+          version: 1,
+        },
+        issuetype: {
+          id: "10007",
+        },
+      },
+    };
+
+    const secret = `${process.env.JIRA_USERNAME}:${process.env.JIRA_PASSWORD}`;
+    try {
+      const res = await axios.post(
+        "https://sarveshpandey221.atlassian.net/rest/api/3/issue",
+        data,
+        {
+          headers: {
+            Authorization: `Basic ${Buffer.from(secret).toString("base64")}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Jira issue created:", res.data);
+    } catch (err) {
+      console.log("Jira API Error:", err.response?.data || err.message);
     }
-})
-
-
-
-
+  }
+});
 
 server.listen(PORT, () => {
-    console.log(`listening on port : ${PORT}`);
-})
+  console.log(`listening on port : ${PORT}`);
+});
