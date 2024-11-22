2169c093eb34ef654f6334b18fc9395f4ea2e5a0
shivamp1998 <shivampandey98@gmail.com>
2024-11-22 13:03:29 +0530

    added open ai handler

--- app.js
+++ app.js
@@ -11,21 +11,34 @@ const token = process.env.BOT_TOKEN;
 const eventsApi = require('@slack/events-api');
 const slackEvents = eventsApi.createEventAdapter(process.env.APP_SIGNINSECRET);
 const { WebClient, LogLevel } = require("@slack/web-api");
+const { ChatOpenAI } = require('@langchain/openai');
 
 const client = new WebClient(token, {
     logLevel: LogLevel.DEBUG
 })
 
+const chatModel = new ChatOpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+})
 //middle wares
 app.use('/', slackEvents.expressMiddleware());
 
 
+const handleCallOpenAi = async (message) => {
+    const response = await chatModel.call([`Reply to this message: ${message}`])
+    console.log(response?.content);
+    return response?.content;
+
+}   
+
 slackEvents.on("message", async (event) => {
+    console.log(event?.subtype, event.bot_id)
+    console.log(event);
     if(!event?.subtype && !event.bot_id) {
         client.chat.postMessage({
             token,
             channel: event.channel,
-            text: 'working fine, i guess?'
+            text: await handleCallOpenAi(event.text) || 'hello'
         })
     }
 })
